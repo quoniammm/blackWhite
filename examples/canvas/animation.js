738ecfee521a655fddf917cbd464221cb55ab42d
quoniammm <quoniamy@gmail.com>
2017-10-11 11:31:33 +0800

    re

---
+++ examples/canvas/animation.js
@@ -0,0 +1,29 @@
+const {Canvas, CheckBox, Page, device} = require('tabris');
+const AnimationExample = require('./AnimationExample');
+
+let animationExample = new AnimationExample();
+
+let page = new Page({
+  title: 'Animation',
+  autoDispose: false
+}).on('disappear', () => {
+  page.children('#animateCheckBox').set('checked', false);
+});
+
+new Canvas({
+  left: 10, top: 10, right: 10, bottom: '#animateCheckBox 10'
+}).on('resize', ({target, width, height}) => {
+  let scaleFactor = device.scaleFactor;
+  let ctx = target.getContext('2d', width * scaleFactor, height * scaleFactor);
+  ctx.scale(scaleFactor, scaleFactor);
+  animationExample.draw(ctx, width, height);
+}).appendTo(page);
+
+new CheckBox({
+  centerX: 0, bottom: 10,
+  text: 'Animate',
+  id: 'animateCheckBox'
+}).on('checkedChanged', ({value: checked}) => animationExample.animating = checked)
+  .appendTo(page);
+
+module.exports = page;
---
+++ examples/web-socket/README.md
@@ -0,0 +1,13 @@
+# Chat example
+
+## Running the example
+
+* Start the chat server by running:
+
+  ```
+  npm run server
+  ```
+
+  The server URL will be output to the console.
+
+* Start the example and enter the server URL when prompted, then confirm.
